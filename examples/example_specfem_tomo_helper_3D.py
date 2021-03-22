d3a220572d2df8754457cd12a7b94cd6b42a4761
thurinj <julienthurin06@gmail.com>
2021-03-22 16:34:26 +0100

    Added 2D case model, and modified the example file.

--- examples/example_specfem_tomo_helper_3D.py
+++ examples/example_specfem_tomo_helper_3D.py
@@ -12,7 +12,7 @@ dx = 10000
 dy = 10000
 z_min = -250  # in km
 z_max = 0  # in km
-dz = 10000 # in m
+dz = 10000  # in m
 
 
 # NO INPUT NEEDED PAST THIS LINE
@@ -28,27 +28,27 @@ vsv = nc_model.load_variable('vsv', fill_nan=False)
 myProj = define_utm_projection(34, 'N')
 # Second mode is here, with graphical area selection for interpolation.
 # The graphical selection tool takes an initial projection as argument. It can be modified using the GUI if it was not correctly specified.
-holder = maptool(nc_model, myProj)
-interpolator = trilinear_interpolator(nc_model, holder.projection)
-interpolator.interpolation_parameters(holder.extent[0], holder.extent[1], dx,
-                                      holder.extent[2], holder.extent[3], dy,
+gui_parameters = maptool(nc_model, myProj)
+interpolator = trilinear_interpolator(nc_model, gui_parameters.projection)
+interpolator.interpolation_parameters(gui_parameters.extent[0], gui_parameters.extent[1], dx,
+                                      gui_parameters.extent[2], gui_parameters.extent[3], dy,
                                       z_min, z_max, dz)
 tomo = interpolator.interpolate([vsv])
 
 # As we want Vp, Vs and Rho to write the specfem files, we perform the following.
-coordinates = tomo[:,0:3]
-vsv = tomo[:,3]
-vp = vs2vp(vsv) # Convert vs to vp with empirical law
-rho = vp2rho(vp) # Converte vp to rho with empirical law
-param = np.vstack((vp, vsv, rho)).T # Stack the three params
+coordinates = tomo[:, 0:3]
+vsv = tomo[:, 3]
+vp = vs2vp(vsv)  # Convert vs to vp with empirical law
+rho = vp2rho(vp)  # Converte vp to rho with empirical law
+param = np.vstack((vp, vsv, rho)).T  # Stack the three params
 
 # Recombine the coordinates and the params in to the tomo array, [lon, lat, depth, vp, vs, rho] required to write.
 tomo = np.hstack((coordinates, param))
-
+# Write the tomography_file.xyz in "./" directory. It uses the tomography array and the interpolator parameters to produce the HEADER.
 write_tomo_file(tomo, interpolator, './')
 
 # Display the interpolated model.
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(tomo[:,0], tomo[:,1], tomo[:,2], c=tomo[:,3])
+ax.scatter(tomo[:, 0], tomo[:, 1], tomo[:, 2], c=tomo[:, 3])
 plt.show()
