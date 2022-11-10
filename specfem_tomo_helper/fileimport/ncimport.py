50979bd979a9db5104fb752a2731f3058ac89ccf
thurinj <julienthurin06@gmail.com>
2022-11-09 21:07:59 -0500

    Changed method for swap dimension order to avoid axis flipping at import

--- specfem_tomo_helper/fileimport/ncimport.py
+++ specfem_tomo_helper/fileimport/ncimport.py
@@ -65,7 +65,8 @@ class Nc_model:
             var_mask = np.array(self.dataset.variables[str(varname)][:, :, :].mask)
             var_data[var_mask] = np.nan
         if dims == ('depth', 'latitude', 'longitude'):
-            var_data = np.rot90(var_data.T,k=1)
+            # Move depth axis to the last column.
+            var_data = np.moveaxis(var_data, 0, -1)
 
         if fill_nan == 'vertical':
             for depth_id in len(self.depth)-2-np.arange(len(self.depth)-1):
