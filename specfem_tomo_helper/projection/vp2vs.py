5bda45d8da27d6079c9dace6b9ae363c60586613
thurinj <julienthurin06@gmail.com>
2022-11-22 22:45:22 -0800

    Addressed ambiguity error for Issue #3

--- specfem_tomo_helper/projection/vp2vs.py
+++ specfem_tomo_helper/projection/vp2vs.py
@@ -10,7 +10,7 @@ def vp2vs(vp):
         P-wave velocity (km/s)
 
     """
-    if vp >= 15:
+    if any(vp) >= 15:
         raise AssertionError('vp values must be in km/s')
     vspeed = vp
     vs = 0.7858 - 1.2344*vspeed + 0.7949*vspeed**2 - 0.1238*vp**3 + 0.0064*vp**4
