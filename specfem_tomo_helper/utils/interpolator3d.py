c9df31e5b5a298439c158a6f448906e04b965633
thurinj <julienthurin06@gmail.com>
2025-03-31 00:52:01 +0900

    Changed the basis for interpolation to allow using RegularGridInterpolator
    Previous versions of the code did not take advantage of how much simpler it is to build the interpolator object on a regular lat/lon/depth grid.
    
    Instead of :
    Projection to UTM > Interpolation
    the code now performs:
    Interpolation > Projection to UTM
    
    This should make the 3D interpolation much faster.
    
    Also added a basic chunking of the interpolation queries, to allow for progress to be printed, and enable easy parallelization in the future

--- specfem_tomo_helper/utils/interpolator3d.py
+++ specfem_tomo_helper/utils/interpolator3d.py
@@ -1,79 +1,136 @@
 import numpy as np
-from scipy.interpolate import LinearNDInterpolator
+from scipy.interpolate import RegularGridInterpolator
 
 class trilinear_interpolator():
-    """ 3D interpolator. """
+    """
+    3D interpolator using RegularGridInterpolator in lat-lon-depth space,
+    with the final output on a uniform UTM x-y-depth grid.
+    """
 
     def __init__(self, model, projection):
-        # Initialise the various paramters of the interpolator, mainly the coordinates that will be used later.
-        self.utm_x = None
-        self.utm_y = None
-        self.tomo_xyz = []
-        self.lon, self.lat, self.depth = model.load_coordinates()
-        self.y, self.x, self.z = np.meshgrid(self.lat, self.lon, self.depth, indexing='ij')
-        utm_x, utm_y = projection(self.x, self.y)
-        self.utm_x = utm_x.flatten()
-        self.utm_y = utm_y.flatten()
+        self.projection = projection
 
-    def interpolation_parameters(self, xspecfem_min, xspecfem_max, dx, yspecfem_min, yspecfem_max, dy, zspecfem_min, zspecfem_max, dz):
-        """ Initialize the interpolation grid from a set of coordinates parameters. """
-        # xspecfem_min/max corresponds to the Longitude min/max in Mesh_Par_File
+        # Load coordinate arrays (1D)
+        self.lon, self.lat, self.depth_km = model.load_coordinates()
+
+        # For RegularGridInterpolator
+        self.lat_array = self.lat
+        self.lon_array = self.lon
+        self.depth_array_m = self.depth_km * 1e3
+
+        # Target grid placeholders
+        self.x_interp_coordinates = None
+        self.y_interp_coordinates = None
+        self.z_interp_coordinates = None
+        self.X_grid = None
+        self.Y_grid = None
+        self.Z_grid = None
+
+        # Also store these for reference
+        self.xspecfem_min = None
+        self.xspecfem_max = None
+        self.dx = None
+        self.yspecfem_min = None
+        self.yspecfem_max = None
+        self.dy = None
+        self.zmin = None
+        self.zmax = None
+        self.dz = None
+
+    def interpolation_parameters(self,
+                                 xspecfem_min, xspecfem_max, dx,
+                                 yspecfem_min, yspecfem_max, dy,
+                                 zspecfem_min, zspecfem_max, dz):
         self.xspecfem_min = xspecfem_min
         self.xspecfem_max = xspecfem_max
-        # dx is the easting sampling rate in m
         self.dx = dx
-        # yspecfem_min/max corresponds to the Latitude min/max in Mesh_Par_File
+
         self.yspecfem_min = yspecfem_min
         self.yspecfem_max = yspecfem_max
-        # dy is the northing sampling rate in m
         self.dy = dy
-        # zmin and zmax are the desired interpolation bounds in km. zmin should be the positive topographic values (the "top" of the model), zmax is the maximum depth (should be negative in the vast majorities of instances).
-        self.zmin = zspecfem_min * 1e3  # Convert to meters
-        self.zmax = zspecfem_max * 1e3  # Convert to meters
-        # dz is the depth sampling rate in m
+
+        self.zmin = zspecfem_min * 1e3
+        self.zmax = zspecfem_max * 1e3
         self.dz = dz
-        # Here are defined the interpolation coordinates as a regular grid in utm coordinates.
-        self.x_interp_coordinates = np.arange(self.xspecfem_min, self.xspecfem_max + self.dx, self.dx)
-        self.y_interp_coordinates = np.arange(self.yspecfem_min, self.yspecfem_max + self.dy, self.dy)
-        self.z_interp_coordinates = np.arange(self.zmin, self.zmax + self.dz, self.dz)
-        self.X_grid, self.Y_grid, self.Z_grid = np.meshgrid(self.x_interp_coordinates, self.y_interp_coordinates, self.z_interp_coordinates, indexing='ij')
 
-    def interpolate(self, model_param):
-        model_values = [param.values.flatten() for param in model_param]
+        self.x_interp_coordinates = np.arange(self.xspecfem_min,
+                                              self.xspecfem_max + self.dx,
+                                              self.dx)
+        self.y_interp_coordinates = np.arange(self.yspecfem_min,
+                                              self.yspecfem_max + self.dy,
+                                              self.dy)
+        self.z_interp_coordinates = np.arange(self.zmin,
+                                              self.zmax + self.dz,
+                                              self.dz)
 
-        utm_x, utm_y = self.utm_x, self.utm_y
-        lon, lat, depth = self.x.flatten(), self.y.flatten(), self.z.flatten() * 1e3
+        self.X_grid, self.Y_grid, self.Z_grid = np.meshgrid(
+            self.x_interp_coordinates,
+            self.y_interp_coordinates,
+            self.z_interp_coordinates,
+            indexing='ij'
+        )
 
-        model_pad_y = np.diff(self.lat[:2])[0] * 111000
-        model_pad_x = np.diff(self.lon[:2])[0] * 111000
+    def interpolate(self, model_param_list, chunk_size=200000):
+        """
+        Interpolate a list of 3D model parameters [param1_3d, param2_3d, ...]
+        each shaped (n_lat, n_lon, n_depth), onto the uniform UTM x-y-depth grid.
 
-        valid_indices = np.where(
-            (utm_y >= self.Y_grid.min() - model_pad_y) &
-            (utm_y <= self.Y_grid.max() + model_pad_y) &
-            (utm_x >= self.X_grid.min() - model_pad_x) &
-            (utm_x <= self.X_grid.max() + model_pad_x)
-        )[0]
+        Optionally, specify `chunk_size` to process queries in batches
+        and print progress for large grids.
 
-        utm_x, utm_y, lon, lat, depth = utm_x[valid_indices], utm_y[valid_indices], lon[valid_indices], lat[valid_indices], depth[valid_indices]
-        model_values = [values[valid_indices] for values in model_values]
+        Returns:
+            final_tomo_xyz : array of shape (N, 3 + n_params)
+        """
+        # Flatten out the target grid in 'F' order
+        xq = self.X_grid.ravel(order='F')
+        yq = self.Y_grid.ravel(order='F')
+        zq = self.Z_grid.ravel(order='F')
 
-        utm_xyz = np.vstack((utm_x, utm_y, depth)).T
+        # Inverse projection: (UTM_x, UTM_y) -> (lon_new, lat_new)
+        lon_new, lat_new = self.projection(xq, yq, inverse=True)
+
+        # Build final coordinate array for RegularGridInterpolator queries
+        query_points = np.column_stack((lat_new, lon_new, zq))
+        n_total = query_points.shape[0]
 
         interpolated_params = []
-        interp_points = np.vstack((self.X_grid.flatten(), self.Y_grid.flatten(), self.Z_grid.flatten())).T
-        for values in model_values:
-            interpolator = LinearNDInterpolator(utm_xyz, values)
-            interpolated_params.append(interpolator(interp_points).reshape(self.X_grid.shape))
 
-        tomo_volume = np.stack(interpolated_params, axis=-1)
+        # For each parameter in model_param_list
+        for param_idx, param in enumerate(model_param_list, start=1):
+            if hasattr(param, "values"):
+                data = param.values
+            else:
+                data = param
 
+            # Build the interpolator for this parameter
+            f = RegularGridInterpolator(
+                (self.lat_array, self.lon_array, self.depth_array_m),
+                data,
+                method='linear',
+                bounds_error=False,
+                fill_value=np.nan
+            )
+
+            # Now do chunked interpolation to show progress
+            vals = np.empty(n_total, dtype=float)
+            for start in range(0, n_total, chunk_size):
+                end = min(start + chunk_size, n_total)
+                vals[start:end] = f(query_points[start:end])
+                # Print simple progress
+                percent = (end / n_total) * 100
+                print(f"  Param {param_idx}/{len(model_param_list)}: "
+                      f"Interpolated {end}/{n_total} points ({percent:.1f}%)")
+
+            interpolated_params.append(vals)
+
+        # Assemble final array
         final_tomo_xyz = np.column_stack((
-            self.X_grid.flatten(order='F'), 
-            self.Y_grid.flatten(order='F'), 
+            self.X_grid.flatten(order='F'),
+            self.Y_grid.flatten(order='F'),
             self.Z_grid.flatten(order='F')
         ))
 
-        for param in interpolated_params:
-            final_tomo_xyz = np.column_stack((final_tomo_xyz, param.flatten(order='F')))
+        for arr in interpolated_params:
+            final_tomo_xyz = np.column_stack((final_tomo_xyz, arr))
 
         return final_tomo_xyz
